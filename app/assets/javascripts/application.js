1f3d9aa3de7cb0b6e2c7e755309a702332ff07dd
Nic <n.f@live.com.au>
2017-03-07 14:59:30 +1100

    improving mobile compatibility and adding coming-soon page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,21 +14,3 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-
-jQuery(document).ready(function($){
- // Defining a function to set size for #hero 
-    function fullscreen(){
-        jQuery('#hero').css({
-            width: jQuery(window).width(),
-            height: jQuery(window).height()
-        });
-    }
-  
-    fullscreen();
-
-  // Run the function in case of window resize
-  jQuery(window).resize(function() {
-       fullscreen();         
-    });
-
-});
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'static_pages/coming_soon'
+
   get 'static_pages/index'
   root 'static_pages#index'
 
