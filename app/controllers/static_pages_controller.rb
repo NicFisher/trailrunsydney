c495b7a56f9d01d454093cea7dc3e55b4c646c58
Nic <n.f@live.com.au>
2017-03-21 08:46:04 +1100

    adding strava_route to static_pages controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,10 +6,11 @@ class StaticPagesController < ApplicationController
     @name = params[:name]
     @email = params[:email]
     @message = params[:message]
+    @strava_route = params[:strava_route]
     ActionMailer::Base.mail(:from => @email,
         :to => 'hello@trailrunsydney.com',
         :subject => "A new contact form message from #{@name}",
-        :body => @message).deliver_now
+        :body => @message, @strava_route).deliver_now
   end
 
 end
