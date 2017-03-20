d57ef35dc0e8d677c73d4e05ad1aed187d712178
Nic <n.f@live.com.au>
2017-03-21 09:08:20 +1100

    updating mailer files

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,11 +6,17 @@ class StaticPagesController < ApplicationController
     @name = params[:name]
     @email = params[:email]
     @message = params[:message]
-    @strava_route = params[:strava_route]
-    ActionMailer::Base.mail(:from => @email,
-        :to => 'hello@trailrunsydney.com',
-        :subject => "A new contact form message from #{@name}",
-        :body => @message).deliver_now
+    UserMailer.contact_form(@email, @name, @message).deliver_now
   end
 
+  def contribute_thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    @strava_route = params[:strava_route]
+    UserMailer.contribute_form(@email, @name, @message, @strava_route).deliver_now
+  end
+
+
 end
+
